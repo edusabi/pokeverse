de9977cf007140812181a8cc2555f04dc6f7d38c
Edu <luis_100404@hotmail.com>
2026-01-07 13:12:47 -0300

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,35 +2,38 @@ import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 
-const gifs = [
-    "https://i.redd.it/v015pnmeg5m81.gif",
-    "https://shinyash.com/cdn/shop/files/0249_-_-Lugia.gif?v=1743342565&width=1013",
-  "https://shinyash.com/cdn/shop/files/0006---Charizard.gif?v=1737774265",
-  "https://i.makeagif.com/media/2-16-2016/wu260n.gif",
-  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg14fsViCCREl8DwLwsKAtt6PLW__8nnCbatddrFqQiVyspQaCbVerqLI7m8rbN2-NakWMuO6hj5bugmhxTMJze2lfaDGhWCeP4yQJasFyV5iG76XNBMU5RLSqEGr4BjIgsPvbtrhKdmv0/s1600/384.gif",
-  "https://cdn.staticneo.com/w/pokemon/8/80/392.gif",
-  "https://shinyash.com/cdn/shop/files/0004_-_-Charmander_ca93207e-35b7-4aa6-98ee-192dc51f2d1e.gif?v=1747272731&width=650",
-  "https://pokefella.com/cdn/shop/files/Mew_800x.gif?v=1613677022",
-  "https://wiki.pokexgames.com/images/7/7f/18_-_Pidgeot.gif",
-  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgtH7-sBV9eSKQAD7XBwHJAlXu1jJHNx5x6NiNGPF1fxYubKLhK-4hzYAC9TaTeqtUKkw4br5gm9LmuZ-cnYbKNtbe4cuRHqxEoE-m91kphvH5Gh4K5_dW09S1jdW57gPlSJRdwuO2gU6HYsO4lYosPbQXbFF6FJZX6CXSc7YNZRqDTrx8-0YLUHj7LUXE/s320/0001%20-%20Bulbasaur.gif",
-  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_nYInnZpAVYqvqzVFbwlyL8MZn98PC1p3Bpvgy4krUiF7w6uFQA7QUnsHDzFd5uSkncVKNozdm8UYW-3Tr7q8k6FSI1YmT2doycbSZLcbGzCPKeov7wOtIfyT-Q3sHNfcTf1LsAbhErk/s1600/Noivern_XY.gif",
-  "https://professorlotus.com/Sprites/Gible.gif",
-  "https://professorlotus.com/Sprites/Gengar.gif",
+const SPRITE_URL = (id) =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
 
-
-];
+const FALLBACK =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
 
 const Navbar = () => {
-  const [selectedGif, setSelectedGif] = useState(() => {
-    return localStorage.getItem("pokemonGif") || gifs[0];
-  });
-
+  const [pokemonList, setPokemonList] = useState([]);
   const [open, setOpen] = useState(false);
 
-  // ✅ EFEITO APENAS PARA SINCRONIZAR COM LOCALSTORAGE
+  const [selectedPokemon, setSelectedPokemon] = useState(() => {
+    const saved = localStorage.getItem("pokemon");
+    return saved ? JSON.parse(saved) : { id: 25, name: "pikachu" };
+  });
+
+  // 🔹 Buscar lista de Pokémon
   useEffect(() => {
-    localStorage.setItem("pokemonGif", selectedGif);
-  }, [selectedGif]);
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then(res => res.json())
+      .then(data => {
+        const formatted = data.results.map((p, index) => ({
+          name: p.name,
+          id: index + 1,
+        }));
+        setPokemonList(formatted);
+      });
+  }, []);
+
+  // 🔹 Salvar no localStorage
+  useEffect(() => {
+    localStorage.setItem("pokemon", JSON.stringify(selectedPokemon));
+  }, [selectedPokemon]);
 
   return (
     <header className={styles.header}>
@@ -39,24 +42,32 @@ const Navbar = () => {
         className={styles.avatar}
         onClick={() => setOpen(!open)}
       >
-        <img src={selectedGif} alt="Pokemon selecionado" />
+        <img
+          src={SPRITE_URL(selectedPokemon.id)}
+          onError={(e) => (e.target.src = FALLBACK)}
+          alt={selectedPokemon.name}
+        />
       </div>
 
       {open && (
         <div className={styles.dropdown}>
-          {gifs
-            .filter(gif => gif !== selectedGif)
-            .map((gif, index) => (
+          {pokemonList.map((pokemon) => (
+            <div
+              key={pokemon.id}
+              className={styles.pokemonOption}
+              onClick={() => {
+                setSelectedPokemon(pokemon);
+                setOpen(false);
+              }}
+            >
               <img
-                key={index}
-                src={gif}
-                alt="pokemon option"
-                onClick={() => {
-                  setSelectedGif(gif);
-                  setOpen(false);
-                }}
+                src={SPRITE_URL(pokemon.id)}
+                onError={(e) => (e.target.src = FALLBACK)}
+                alt={pokemon.name}
               />
-            ))}
+              <span>{pokemon.name}</span>
+            </div>
+          ))}
         </div>
       )}
 
